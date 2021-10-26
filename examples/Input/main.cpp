45a54d57210b9ae2bfa079b6f482708370bb5e26
tMario2111 <76879527+tMario2111@users.noreply.github.com>
2021-10-26 17:01:25 +0300

    Added Input example

---
+++ examples/Input/main.cpp
@@ -0,0 +1,41 @@
+#include <SFML/Graphics.hpp>
+#include "../../MKE/src/MKE/Input.h"
+#include <iostream>
+
+int main()
+{
+	sf::RenderWindow win(sf::VideoMode(800, 800), "Input example", sf::Style::Close);
+	win.setVerticalSyncEnabled(true);
+
+	mke::Input input;
+
+	while (win.isOpen())
+	{
+		input.update(); // update the input at the start of each frame
+		for (sf::Event event; win.pollEvent(event);)
+		{
+			input.processEvent(event); // process key and mouse events
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				win.close();
+				break;
+			default:
+				break;
+			}
+		}
+
+		if (input.isKeyPressed(sf::Keyboard::Space))
+			std::cout << "Space pressed\n";
+		else if (input.isKeyReleased(sf::Keyboard::Space))
+			std::cout << "Space released\n";
+		if (input.isMouseButtonPressed(sf::Mouse::Left))
+			std::cout << "Left mouse button pressed\n";
+		else if (input.isMouseButtonReleased(sf::Mouse::Left))
+			std::cout << "Left mouse button released\n";
+
+		win.clear();
+		win.display();
+	}
+	return 0;
+}
